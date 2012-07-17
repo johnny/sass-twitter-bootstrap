06795315e0476e1f64c991d6686a21c16c68423e
Jonas von Andrian <jvadev@gmail.com>
2012-07-17 16:43:59 +0200

    better loading and fixed root scss files

--- lib/sass-twitter-bootstrap.rb
+++ lib/sass-twitter-bootstrap.rb
@@ -2,22 +2,14 @@ module Bootstrap
   class FrameworkNotFound < StandardError; end
 
   def self.load!
-    if compass?
-      register_compass_extension
+    styles = File.dirname(__FILE__)
+    if Sass.respond_to? :load_paths
+      Sass.load_paths << styles
+    elsif defined?(::Compass)
+      ::Compass::Frameworks.register('bootstrap', :stylesheets_directory => styles)
     else
-      raise Bootstrap::FrameworkNotFound, "bootstrap-sass requires either Rails > 3.1 or Compass, neither of which are loaded"
+      raise Bootstrap::FrameworkNotFound, "sass-twitter-bootstrap requires either Sass master or Compass, neither of which are loaded"
     end
-  end
-
-  private
-  def self.compass?
-    defined?(::Compass)
-  end
-
-  def self.register_compass_extension
-    base = File.join(File.dirname(__FILE__), '..')
-    styles = File.join(base, 'lib')
-    ::Compass::Frameworks.register('bootstrap', :stylesheets_directory => styles)
   end
 end
 
